76f9164e140eb89d164d8f5999de9c6bea438a29
Daniel Motz <hello@daniel-motz.de>
2023-11-16 18:52:45 +0100

    Add example CQ Overview Component and View

---
+++ src/components/CompetencyQuestionListItem.vue
@@ -0,0 +1,44 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+import { StarIcon } from '@heroicons/vue/24/solid'
+
+export default defineComponent({
+  name: "CompetencyQuestionListItem",
+
+  props: {
+    text: String,
+    identifier: Number,
+    creator: String,
+    rating: Number
+  },
+
+  components: {StarIcon}
+})
+</script>
+
+<template>
+
+  <div class="bg-gray-700 mt-10 rounded p-4 text-left">
+
+    <div class="grid grid-cols-2">
+      <h3 class="font-bold">{{ text }}</h3>
+      <div class="inline-block text-right">
+        <StarIcon v-for="_ in rating" class="h-5 w-5 text-yellow-500 inline" aria-hidden="true" />
+      </div>
+    </div>
+
+
+    <code>identifier: {{ identifier }}</code>
+
+    <p>
+      {{ creator }}
+    </p>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/CompentencyQuestionOverviewView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CompetencyQuestionListItem from "../components/CompetencyQuestionListItem.vue";
+
+const cqs = [
+  { text: "What is the topping of a Hawaii pizza?", creator: "Daniel", identifier: 3, rating: 2 },
+  { text: "What is the topping of a Diavolo pizza?", creator: "Daniel", identifier: 43, rating: 5 },
+  { text: "Whatasdasd", creator: "asdasd", identifier: 123, rating: 3 }
+]
+</script>
+
+<template>
+  <CompetencyQuestionListItem v-for="cq in cqs" :text="cq.text" :creator="cq.creator" :identifier="cq.identifier" :rating="cq.rating" />
+
+</template>
+
+<style scoped>
+
+</style>
