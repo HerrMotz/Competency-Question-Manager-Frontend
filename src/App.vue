f451b979598447217b882aff1bb31cfb1f5af375
Daniel Motz <hello@danielmotz.de>
2023-11-20 15:28:33 +0100

    Add Store, Types, Router, Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
-import CompentencyQuestionOverviewView from "./views/CompentencyQuestionOverviewView.vue";
+
+import Navbar from "./components/Navbar.vue";
+
+import { useStore } from "./store.ts";
+import { storeToRefs } from 'pinia'
+const useStore1 = useStore()
+const { getUser, isLoggedIn } = storeToRefs(useStore1)
+
 </script>
 
 <template>
-  <CompentencyQuestionOverviewView />
+  <div>
+
+
+    <Navbar :user="getUser" :is-logged-in="isLoggedIn" class="mb-10" />
+
+    <main class="py-10 lg:pl-72">
+      <div class="px-4 sm:px-6 lg:px-8">
+        <RouterView />
+      </div>
+    </main>
+  </div>
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
