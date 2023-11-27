e31fe578ada6140536abde85050d7dedcbeb91db
Daniel Motz <hello@daniel-motz.de>
2023-11-27 09:33:07 +0100

    Add custom logo (WIP)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -42,6 +42,12 @@ export default defineComponent({
     TransitionRoot,
   },
 
+  computed: {
+    currentRouteName() {
+      return this.$route.name;
+    }
+  },
+
   data() {
     return {
       sidebarOpen: false,
@@ -53,10 +59,10 @@ export default defineComponent({
       ],
 
       navigation: [
-        { name: 'Competency Questions', href: '#', icon: ListBulletIcon, current: true },
-        { name: 'Team', href: '#', icon: UsersIcon, current: false },
-        { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-        { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+        { name: 'Competency Questions', href: '/questions', icon: ListBulletIcon, current: false },
+        { name: 'Team', href: '/team', icon: UsersIcon, current: false },
+        { name: 'Projects', href: '/projects', icon: FolderIcon, current: false },
+        { name: 'Reports', href: '/reports', icon: ChartPieIcon, current: false },
       ],
     }
   }
@@ -91,8 +97,8 @@ export default defineComponent({
                   <li>
                     <ul role="list" class="-mx-2 space-y-1">
                       <li v-for="item in navigation" :key="item.name">
-                        <RouterLink :to="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                          <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+                        <RouterLink :to="item.href" :class="[item.href === currentRouteName ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                          <component :is="item.icon" :class="[item.href === currentRouteName ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
                           {{ item.name }}
                         </RouterLink>
                       </li>
@@ -123,7 +129,7 @@ export default defineComponent({
     <!-- Sidebar component, swap this element with another sidebar if you like -->
     <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6">
       <div class="flex h-16 shrink-0 items-center">
-        <span class="text-2xl font-bold text-white mt-5">CQ-Manager</span>
+        <span class="text-2xl font-bold text-white mt-5"><img src="/cq-manager.svg" class="h-10 fill-amber-50" alt="CQ-Manager"></span>
       </div>
       <nav class="flex flex-1 flex-col">
         <ul role="list" class="flex flex-1 flex-col gap-y-7">
