f6e3b3c135919217735080d5756a27ac923b213c
Daniel Motz <hello@danielmotz.de>
2023-11-30 12:03:46 +0100

    Fix router and active link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -44,7 +44,7 @@ export default defineComponent({
 
   computed: {
     currentRouteName() {
-      return this.$route.name;
+      return this.$route.path;
     }
   },
 
@@ -108,10 +108,10 @@ export default defineComponent({
                     <div class="text-xs font-semibold leading-6 text-indigo-200">Your teams</div>
                     <ul role="list" class="-mx-2 mt-2 space-y-1">
                       <li v-for="team in teams" :key="team.name">
-                        <a :href="team.href" :class="[team.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                        <RouterLink :to="team.href" :class="[team.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
                           <span class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">{{ team.initial }}</span>
                           <span class="truncate">{{ team.name }}</span>
-                        </a>
+                        </RouterLink>
                       </li>
                     </ul>
                   </li>
@@ -149,10 +149,10 @@ export default defineComponent({
           <li>
             <ul role="list" class="-mx-2 space-y-1">
               <li v-for="item in navigation" :key="item.name">
-                <a :href="item.href" :class="[item.current ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                  <component :is="item.icon" :class="[item.current ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+                <RouterLink :to="item.href" :class="[currentRouteName == item.href ? 'bg-indigo-700 text-white' : 'text-indigo-200 hover:text-white hover:bg-indigo-700', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                  <component :is="item.icon" :class="[currentRouteName == item.href ? 'text-white' : 'text-indigo-200 group-hover:text-white', 'h-6 w-6 shrink-0']" aria-hidden="true" />
                   {{ item.name }}
-                </a>
+                </RouterLink>
               </li>
             </ul>
           </li>
