a500548a8bbee2770be0e8dd6e425b69afe2fd5a
Daniel Motz <hello@danielmotz.de>
2023-11-30 16:31:27 +0100

    Move logo to component

---
+++ src/components/Logo.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 class="bg-gradient-to-r text-transparent bg-clip-text from-indigo-500 dark:from-indigo-400 via-purple-500
+      dark:via-purple-400 to-pink-500 dark:to-pink-400 font-black text-center">
+    CQ-Manager
+  </h1>
+</template>
