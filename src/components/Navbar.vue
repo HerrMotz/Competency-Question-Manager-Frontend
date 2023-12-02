1f729e926e4168cc863252d1a7bd69f811dbe23f
Daniel Motz <hello@daniel-motz.de>
2023-12-02 10:15:02 +0100

    Fix profile button width

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -163,7 +163,7 @@ export default defineComponent({
             </ul>
           </li>
           <li class="-mx-6 mt-auto">
-            <button @click="store.logout(); $router.push('/login');" class="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700">
+            <button @click="store.logout(); $router.push('/login');" class="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700 w-full">
               <img class="h-8 w-8 rounded-full bg-indigo-700" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
               <span class="sr-only">Your profile</span>
               <span aria-hidden="true">{{ user.name }}</span>
