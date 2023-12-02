bf740419aeb1c20e0a29f4bc285d411e7f17c53b
Daniel Motz <hello@daniel-motz.de>
2023-12-02 10:24:32 +0100

    Add TODO for token validity

--- src/services/authHeader.ts
+++ src/services/authHeader.ts
@@ -5,6 +5,9 @@ const { getUser } = useStore1
 export default function authHeader() {
     const user = getUser
 
+    // TODO: add a check for token validity
+    //  either check for the timestamp or "ping" using the current token (if a 401 is returned the token is invalid)
+
     if (user && user.accessToken) {
         return { 'Authorization': user.accessToken };
     } else {
