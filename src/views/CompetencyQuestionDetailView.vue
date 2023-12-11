d27de17e28d06cd77ff1b36f753808c4aa3747fa
Daniel Motz <hello@daniel-motz.de>
2023-12-11 09:39:24 +0100

    Reformat

--- src/views/CompetencyQuestionDetailView.vue
+++ src/views/CompetencyQuestionDetailView.vue
@@ -38,17 +38,17 @@ if ("messageType" in response) {
                 :open="messagePopupData.open"
                 @close="messagePopupData.open = false;"/>
   <h1 class="text-2xl">
-    {{cq.data.question}}
+    {{ cq.data.question }}
     <span class="inline-flex items-center rounded-md dark:bg-blue-400/10 px-2 py-1 text-xs font-medium dark:text-blue-400 ring-1 ring-inset dark:ring-blue-400/30
                   bg-blue-50 text-blue-700 ring-blue-700/10">
-      Version: {{cq.data.version}}
+      Version: {{ cq.data.version }}
     </span>
     <span class="inline-block text-right ml-2">
-      <StarIcon v-for="_ in cq.data.rating" class="h-5 w-5 text-yellow-500 inline" aria-hidden="true" />
-      <StarIcon v-for="_ in (5 - cq.data.rating)" class="h-5 w-5 text-gray-500 inline" aria-hidden="true" />
+      <StarIcon v-for="_ in cq.data.rating" class="h-5 w-5 text-yellow-500 inline" aria-hidden="true"/>
+      <StarIcon v-for="_ in (5 - cq.data.rating)" class="h-5 w-5 text-gray-500 inline" aria-hidden="true"/>
     </span>
   </h1>
-  <pre class="text-sm">{{cq.data.id}}</pre>
+  <pre class="text-sm">{{ cq.data.id }}</pre>
 </template>
 
 <style scoped>
