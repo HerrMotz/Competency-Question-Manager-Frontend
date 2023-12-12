8d67321eccd5b7595cf92a58af7f0711b24110e4
Daniel Motz <hello@daniel-motz.de>
2023-12-12 01:44:53 +0100

    Add a consolidation dashboard view

--- src/router.ts
+++ src/router.ts
@@ -23,6 +23,11 @@ const routes =  [
         component: () => import("./views/CompetencyQuestionCreateView.vue")
     },
     {
+        path: "/consolidation/",
+        name: "consolidation",
+        component: () => import("./views/ConsolidationDashboardView.vue")
+    },
+    {
         path: "/team",
         name: "team",
         component: () => import("./views/TeamDashboardView.vue")
