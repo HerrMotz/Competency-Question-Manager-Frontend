890e74dd28e8dde726489c3035fe873ad694a9f6
Chiara Tunc <chiara.tunc@uni-jena.de>
2023-12-17 21:04:00 +0100

    Deleted AddProject Component, Minor design changes in AddProject View

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -3,6 +3,7 @@ import MessagePopup from "../components/MessagePopup.vue";
 import AddProjectDataService from "../services/AddProjectDataService.ts";
 import SaveButtonWithCallback from "../components/SubmitButtonWithCallback.vue";
 import {ArrowDownOnSquareIcon} from "@heroicons/vue/24/solid";
+import EmailTagInput from "../components/EmailTagInput.vue";
 
 export default{
   name: "AddProject",
@@ -53,24 +54,25 @@ export default{
                   @close="messagePopupData.open = false;"/>
     <h1 class="text-2xl">
     </h1>
-    Add a new project
+    Add new project
     <div class="my-5">
-      <label for="project" class="block text-sm font-medium leading-6 dark:text-gray-100 text-gray-900">Add Project:</label>
+      <label for="project" class="block text-sm font-medium leading-6 dark:text-gray-100 text-gray-900">Type in project title:</label>
       <div class="mt-2">
-        <textarea v-model="add.project" rows="4" name="project_name" id="project" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        <textarea v-model="add.project" rows="1" name="project_name" id="project" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       </div>
     </div>
-
-    <SaveButtonWithCallback agree-button-text="Save this question?"
-                            title="Are you sure you want to add the question?"
+   
+    <SaveButtonWithCallback agree-button-text="Save this project?"
+                            title="Are you sure you want to add this project?"
                             detail="Check for typos."
                             @modalsuccessclose="save()">
       <ArrowDownOnSquareIcon class="-ml-0.5 h-5 w-5" aria-hidden="true"/>
-      Add
+      Save
     </SaveButtonWithCallback>
   </div>
 </template>
 
 <style scoped>
-
 </style>
+
+
