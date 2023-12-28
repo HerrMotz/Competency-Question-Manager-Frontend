61f9f310e98c68bd84b78e0232de3842ce7fe5d4
Chiara Tunc <chiara.tunc@uni-jena.de>
2023-12-28 16:12:50 +0100

    Added placeholder

--- src/views/ProjectCreateView.vue
+++ src/views/ProjectCreateView.vue
@@ -85,7 +85,7 @@ export default{
             <button @click="removeTag(index)" class= "tag-remove" aria-label= "Remove tag">x</button>
         </div>
         <div class="mt-2">
-            <textarea v-model="tagValue" @keydown.enter="addTag" rows="5" name="project_add" id="project" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            <textarea v-model="tagValue" @keydown.enter="addTag" placeholder= "Enter e-mail address" rows="5" name="project_add" id="project" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
         </div>
     </div>
     <div class="button-container">
@@ -101,7 +101,7 @@ export default{
 </div>   
 </template>
 
-<style lang= css scoped>
+<style scoped>
 
 
 .tag-input {
