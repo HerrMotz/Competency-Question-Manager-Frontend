bbbef6c31356440103e758d4c34c401966f7a9f3
Daniel Motz <hello@danielmotz.de>
2024-01-09 09:29:26 +0100

    WIP Query builder but first working draggable version

--- src/components/CompetencyQuestionQueryBuilder.vue
+++ src/components/CompetencyQuestionQueryBuilder.vue
@@ -8,7 +8,7 @@ type WordT = {
   isNew: Boolean
 }
 
-const words: Ref<UnwrapRef<(WordT)[]>> = ref([
+const words = ref([
   {index: 1, text: "This", isNew: true},
   {index: 2, text: "is", isNew: true},
   {index: 3, text: "an", isNew: true},
@@ -23,24 +23,23 @@ const drag = ref(false);
 <template>
   <div>
     <draggable
-        class="flex gap-4"
         v-model="words"
-        group="people"
+        class="flex gap-4"
         @start="drag=true"
         @end="drag=false"
         item-key="id">
-      <template #item="{word}">
+      <template #item="{element}">
         <div class="items-center rounded-md px-2 py-1 font-medium ring-1 ring-inset"
-             :class="word.isNew ? 'bg-green-50 text-green-600 ring-green-500/10' : 'bg-gray-50 text-gray-600 ring-ray-500/10'">
-          {{ word.text }}
+             :class="element.isNew ? 'bg-green-50 text-green-600 ring-green-500/10' : 'bg-gray-50 text-gray-600 ring-ray-500/10'">
+          {{ element.text }}
           <button type="button"
                   class="group relative -mr-1 h-3.5 w-3.5 rounded-sm"
-                  :class="word.isNew ? 'hover:bg-green-500/20' : 'hover:bg-gray-500/20'"
-                  @click="words.splice(words.findIndex(e => e.index === word.index), 1)">
+                  :class="element.isNew ? 'hover:bg-green-500/20' : 'hover:bg-gray-500/20'"
+                  @click="words.splice(words.findIndex(e => e.index === element.index), 1)">
             <span class="sr-only">Remove</span>
             <svg viewBox="0 0 14 14"
                  class="h-3.5 w-3.5"
-                 :class="word.isNew ? 'stroke-green-600/50 group-hover:stroke-green-600/75' : 'stroke-gray-600/50 group-hover:stroke-gray-600/75'">
+                 :class="element.isNew ? 'stroke-green-600/50 group-hover:stroke-green-600/75' : 'stroke-gray-600/50 group-hover:stroke-gray-600/75'">
               <path d="M4 4l6 6m0-6l-6 6"/>
             </svg>
             <span class="absolute -inset-1"/>
