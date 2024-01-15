41c9349cb709fa2f0c062a5a63ee54bf2af62267
Daniel Motz <hello@daniel-motz.de>
2024-01-15 08:33:17 +0100

    Change from "Name" to "question"

--- src/views/ConsolidationDetailView.vue
+++ src/views/ConsolidationDetailView.vue
@@ -99,7 +99,7 @@ async function removeQuestions(consolidationId: string, questionIds: string[]) {
     <div>
       <div class="mt-10">
         <input type="text" name="question" id="question"
-               placeholder="No name given"
+               placeholder="No question given"
                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                :value="consolidation.data.name"
         />
