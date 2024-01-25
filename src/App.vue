669640bbdc983a59f14cebcd522054e6da757562
Daniel Motz <hello@daniel-motz.de>
2024-01-25 21:17:22 +0100

    Fix navbar visible on wrong routes

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,16 @@ import Navbar from "./components/Navbar.vue";
 
 import { useStore } from "./store.ts";
 import { storeToRefs } from 'pinia'
+import {computed} from "vue";
+import {useRoute} from "vue-router";
 const useStore1 = useStore()
 const { isLoggedIn } = storeToRefs(useStore1)
+
+const route = useRoute();
+
+const showNavbar = computed(() =>  {
+  return isLoggedIn && route.path !== '/' && route.path !== '/login'
+})
 
 </script>
 
@@ -13,11 +21,12 @@ const { isLoggedIn } = storeToRefs(useStore1)
   <div class="min-h-screen">
 
 
-    <Navbar v-if="isLoggedIn" />
+    <Navbar v-if="showNavbar"/>
 
-    <main class="pb-10 pt-7 min-h-screen dark:bg-gray-800 dark:text-gray-100" :class="[isLoggedIn ? 'lg:pl-72' : '']">
+    <main class="pb-10 pt-7 min-h-screen dark:bg-gray-800 dark:text-gray-100" :class="[showNavbar ? 'lg:pl-72' : '']">
       <div class="px-4 sm:px-6 lg:px-8">
         <Suspense>
+
           <RouterView />
         </Suspense>
       </div>
